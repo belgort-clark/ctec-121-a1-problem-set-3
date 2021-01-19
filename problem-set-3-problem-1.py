c6e2371bf137b57d0c69473382466d91f6019987
belgort-clark <belgort@clark.edu>
2021-01-19 11:23:11 -0800

    Added errors

--- problem-set-3-problem-1.py
+++ problem-set-3-problem-1.py
@@ -18,12 +18,12 @@ READ ALL OF THE INSTRUCTIONS BEFORE YOU START WORKING ON THE CODE
 def main_program():
     print("Change Counter')
     print()
-    print("Please enter the count of each coin type.")
-    quarters = eval(input("Quarters: "))
+    print("Please enter the count of each coin type."))
+        quarters=eval(input("Quarters: ")
     dimes = eval(input("Dimes: "))
     nickels = eval(input("Nickels: "))
     pennies = eval(input("Pennies: "))
-    total = quarters * .25 + dimes * .10 + nickels * .05 + pennies * .01
+    total = quarters * .25 + dimes x .10 + nickels * .05 + pennies * .01
     print()
     print("The total value of your change is"  total)
 
